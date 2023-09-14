8e3ed7eec9b59e1060902f5e7aaddc82cc04d633
Marvin Pacis <108392678+marventures@users.noreply.github.com>
2023-09-14 22:16:21 +0800

    Update example-03.js

--- js/example-03.js
+++ js/example-03.js
@@ -15,11 +15,11 @@ const userInput = prompt('Enter the number');
 
 if (userInput === null) {
   console.log('Your input field is empty');
-} else if (Number(userInput === 0)) {
+} else if (Number(userInput) === 0) {
   console.log('This is zero');
-} else if (Number(userInput > 0)) {
+} else if (Number(userInput) > 0) {
   console.log('This is a positive number!');
-} else if (Number(userInput < 0)) {
+} else if (Number(userInput) < 0) {
   console.log('This is a negative number!');
 } else {
   console.log('Please input a number!');
