00f8e319eb3a1ee50809b3254204e7439fa66125
Marvin Pacis <108392678+marventures@users.noreply.github.com>
2023-10-26 10:07:47 +0800

    Update example-01.js

--- js/example-01.js
+++ js/example-01.js
@@ -6,5 +6,5 @@ const showCT = document.getElementById('showCT');
 showCT.addEventListener('click', event => {
   console.log(event); //
   console.log(event.type); // event type // in this case it's the click event
-  console.log(event.currentTarget); // event.currentTarget -> is the element on which the event handler is executed
+  console.log(event.currentTarget); // event.currentTarget -> is the element on which the event handler is CURRENTLY executed/processed
 });
