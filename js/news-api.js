d4aaf906c321bef81de686484bf5567b0b3ffa1d
Marvin Pacis <108392678+marventures@users.noreply.github.com>
2023-11-21 20:15:28 +0800

    Update code

--- js/news-api.js
+++ js/news-api.js
@@ -1,6 +1,6 @@
 const BASE_URL = 'https://newsapi.org/v2/everything';
 // change this to your API Key
-// const API_KEY = '8219ad0dd6eb470e8be6c4057caf094e';
+// const API_KEY = '';
 
 export function getNews(query) {
   return fetch(`${BASE_URL}?q=${query}&apiKey=${API_KEY}`).then(res => {
